8b37f9e450fc05a8eaab6409d84684855b9b0c78
SSS-Vir <danil.vitt@gmail.com>
2022-07-09 16:58:48 +0500

    supermassive changes

--- src/help.rs
+++ src/help.rs
@@ -2,5 +2,6 @@ pub fn usage_message() {
     println!("usage: hello FILE_PATH [OPTIONS]");
     println!("Options for .gif files:");
     println!("  --fps=FPS\tGif's FPS (default 15)");
+    println!("General options:");
     println!("  --colored\tMakes ascii colored");
 }
