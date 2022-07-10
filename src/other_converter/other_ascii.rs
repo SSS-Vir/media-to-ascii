b05dbf16aa129ced95ea66182aba7971b8bad4e3
SSS-Vir <danil.vitt@gmail.com>
2022-07-10 05:00:26 +0500

    minirefact

--- src/other_converter/other_ascii.rs
+++ src/other_converter/other_ascii.rs
@@ -1,6 +1,5 @@
 use crate::utils::ascii_utils::ImageBufferExtensions;
-use crate::utils::path_utils::{get_file_extension, get_file_name};
-use std::path::Path;
+use crate::utils::path_utils::get_file_name;
 use std::{fs::File, io::Write};
 
 use super::arg_parser::args_parse;
@@ -26,7 +25,6 @@ pub fn picture_to_ascii(args: &Vec<String>) -> Result<(), Box<String>> {
         output.push_str("\\");
         output.push_str(get_file_name(file_path).as_str());
         output.push_str(".txt");
-        println!("Opening {}", output);
         match File::create(&output) {
             Ok(x) => x,
             Err(_) => {
@@ -34,7 +32,6 @@ pub fn picture_to_ascii(args: &Vec<String>) -> Result<(), Box<String>> {
             }
         }
     } else {
-        println!("Opening existing {}", output);
         match std::fs::File::create(&output) {
             Ok(x) => x,
             Err(_) => {
@@ -48,8 +45,14 @@ pub fn picture_to_ascii(args: &Vec<String>) -> Result<(), Box<String>> {
     }
 
     if colored {
+        let new_width = file.width();
         let image_str = file.to_colored_ascii();
         for line in image_str {
+            if !noprint {
+                for _ in 0..(new_width / 2) {
+                    print!(" ");
+                }
+            }
             for c in line {
                 match out_file.write_all(c.as_bytes()) {
                     Ok(_) => {}
@@ -59,10 +62,14 @@ pub fn picture_to_ascii(args: &Vec<String>) -> Result<(), Box<String>> {
                         );
                     }
                 };
+                if !noprint {
+                    print!("{}", c);
+                }
             }
         }
     } else {
         let image_str = file.to_ascii_string();
+        let new_width = file.width();
         for line in image_str {
             match out_file.write_all(line.as_bytes()) {
                 Ok(_) => {}
@@ -70,28 +77,11 @@ pub fn picture_to_ascii(args: &Vec<String>) -> Result<(), Box<String>> {
                     return Err(format!("Error while writing in file: {}", err.to_string()).into());
                 }
             };
-        }
-    }
-
-    if !noprint {
-        let new_width = file.height();
-        if !colored {
-            let image_str = file.to_ascii_string();
-            for line in image_str {
+            if !noprint {
                 for _ in 0..(new_width / 2) {
                     print!(" ");
                 }
                 print!("{}", line);
-            }
-        } else {
-            let image_str = file.to_colored_ascii();
-            for line in image_str {
-                for _ in 0..(new_width / 2) {
-                    print!(" ");
-                }
-                for c in line {
-                    print!("{}", c);
-                }
             }
         }
     }
